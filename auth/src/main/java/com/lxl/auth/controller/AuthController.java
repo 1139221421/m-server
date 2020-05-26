a3fc61a12cee70fe7a433dbfff6b35e7b8ba1038
lixiaolong <lixiaolong@peicent.com>
2020-05-26 14:42:30 +0800

    删除熔断

--- auth/src/main/java/com/lxl/auth/controller/AuthController.java
+++ auth/src/main/java/com/lxl/auth/controller/AuthController.java
@@ -24,27 +24,8 @@ public class AuthController implements ProducerDeal {
     @Autowired
     private RocketMqConsumer rocketMqConsumer;
 
-    /**
-     * 熔断
-     *
-     * @return
-     */
-    @HystrixCommand(
-            groupKey = "testGroup", // 一组 Hystrix 命令的集合， 用来统计、报告，默认取类名，可不配置
-            commandKey = "testCommand", //用来标识一个 Hystrix 命令，默认会取被注解的方法名
-            fallbackMethod = "fallback", // 超时调用方法
-            commandProperties = { // 与此命令相关的属性
-                    @HystrixProperty(name = "execution.timeout.enabled", value = "true"), // 是否给方法执行设置超时，默认为 true
-                    @HystrixProperty(name = "execution.isolation.thread.interruptOnTimeout", value = "3000") // 方法执行超时时间，默认值是 1000，即 1秒
-            }
-    )
     @GetMapping("/test")
     public String test() {
-//        try {
-//            Thread.sleep(2000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
         logger.info("auth 访问...");
 //        rocketMqConsumer.sendTransactionMsg("mq调用测试", MqTagsEnum.TEST);
         return messageFeign.test();
