080d69f936891222f7dbd360aeb6bbd04c38eb54
lixiaolong <lixiaolong@peicent.com>
2020-06-22 12:47:21 +0800

    屏蔽路由规则

--- gateway/src/main/java/com/lxl/gateway/websocket/CustomerLoadBalancerRule.java
+++ gateway/src/main/java/com/lxl/gateway/websocket/CustomerLoadBalancerRule.java
@@ -1,40 +1,40 @@
-package com.lxl.gateway.websocket;
-
-import java.util.List;
-
-import com.netflix.loadbalancer.ClientConfigEnabledRoundRobinRule;
-import com.netflix.loadbalancer.Server;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * 自定义gateway负载均衡，websocket协议单独处理
- * 本来是实现 AbstractLoadBalancerRule，普通请求无法使用默认的路由方式 super.choose(key)
- *
- * @author: lxl
- */
-@Configuration
-public class CustomerLoadBalancerRule extends ClientConfigEnabledRoundRobinRule {
-
-    @Override
-    public Server choose(Object key) {
-        if (key != null && !"default".equals(key.toString())) {
-            List<Server> servers = this.getLoadBalancer().getReachableServers();
-            return consistentHashChoose(servers, key);
-        } else {
-            // 使用默认路由规则
-            return super.choose(key);
-        }
-    }
-
-    /**
-     * websocket采用一致性hash算法来确定与哪个服务建立连接
-     *
-     * @param servers
-     * @param key
-     */
-    private Server consistentHashChoose(List<Server> servers, Object key) {
-        ConsistentHash<Server> consistentHash = new ConsistentHash<>(200, servers);
-        return consistentHash.get(key.toString());
-    }
-
-}
+//package com.lxl.gateway.websocket;
+//
+//import java.util.List;
+//
+//import com.netflix.loadbalancer.ClientConfigEnabledRoundRobinRule;
+//import com.netflix.loadbalancer.Server;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * 自定义gateway负载均衡，websocket协议单独处理
+// * 本来是实现 AbstractLoadBalancerRule，普通请求无法使用默认的路由方式 super.choose(key)
+// *
+// * @author: lxl
+// */
+//@Configuration
+//public class CustomerLoadBalancerRule extends ClientConfigEnabledRoundRobinRule {
+//
+//    @Override
+//    public Server choose(Object key) {
+//        if (key != null && !"default".equals(key.toString())) {
+//            List<Server> servers = this.getLoadBalancer().getReachableServers();
+//            return consistentHashChoose(servers, key);
+//        } else {
+//            // 使用默认路由规则
+//            return super.choose(key);
+//        }
+//    }
+//
+//    /**
+//     * websocket采用一致性hash算法来确定与哪个服务建立连接
+//     *
+//     * @param servers
+//     * @param key
+//     */
+//    private Server consistentHashChoose(List<Server> servers, Object key) {
+//        ConsistentHash<Server> consistentHash = new ConsistentHash<>(200, servers);
+//        return consistentHash.get(key.toString());
+//    }
+//
+//}
--- gateway/src/main/java/com/lxl/gateway/websocket/LoadBalancedBean.java
+++ gateway/src/main/java/com/lxl/gateway/websocket/LoadBalancedBean.java
@@ -1,27 +1,27 @@
-package com.lxl.gateway.websocket;
-
-import com.lxl.gateway.websocket.CustomerLoadBalancerClientFilter;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.cloud.gateway.config.LoadBalancerProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-
-/**
- * 配置使用自定义LoadBalancerRule
- */
-@Configuration
-public class LoadBalancedBean {
-
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
-    @Bean
-    public CustomerLoadBalancerClientFilter userLoadBalanceClientFilter(LoadBalancerClient client, LoadBalancerProperties properties) {
-        return new CustomerLoadBalancerClientFilter(client, properties);
-    }
-}
+//package com.lxl.gateway.websocket;
+//
+//import com.lxl.gateway.websocket.CustomerLoadBalancerClientFilter;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+//import org.springframework.cloud.gateway.config.LoadBalancerProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.client.RestTemplate;
+//
+///**
+// * 配置使用自定义LoadBalancerRule
+// */
+//@Configuration
+//public class LoadBalancedBean {
+//
+//    @Bean
+//    @LoadBalanced
+//    public RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
+//
+//    @Bean
+//    public CustomerLoadBalancerClientFilter userLoadBalanceClientFilter(LoadBalancerClient client, LoadBalancerProperties properties) {
+//        return new CustomerLoadBalancerClientFilter(client, properties);
+//    }
+//}
