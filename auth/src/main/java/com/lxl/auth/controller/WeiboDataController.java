c818a6b02edc470bab87edb7a995928d211f57af
lixiaolong <lixiaolong@peicent.com>
2020-08-09 17:20:30 +0800

    es查询

--- auth/src/main/java/com/lxl/auth/controller/WeiboDataController.java
+++ auth/src/main/java/com/lxl/auth/controller/WeiboDataController.java
@@ -16,7 +16,6 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.sort.SortOrder;
@@ -40,10 +39,8 @@ public class WeiboDataController {
 
     @RequestMapping(value = "/sync")
     public ResponseInfo sync() {
-        Spider.create(new WeiboPageProcessor())
-                .addUrl("https://m.weibo.cn/api/container/getIndex?containerid=102803_ctg1_8999_-_ctg1_8999_home")
-                .addPipeline(new WeiboPipeline())
-                .run();
+        Spider.create(new WeiboPageProcessor()).addUrl("https://m.weibo.cn/api/container/getIndex?containerid" +
+                "=102803_ctg1_8999_-_ctg1_8999_home").addPipeline(new WeiboPipeline()).run();
         List<WeiboData> list = WeibodataList.getDatas();
         if (CollectionUtil.isNotEmpty(list)) {
             elasticCustomerOperate.save(list);
@@ -54,33 +51,30 @@ public class WeiboDataController {
     @RequestMapping(value = "/search")
     public ResponseInfo search(SearchVo searchVo) {
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-        searchSourceBuilder.sort("createTime", SortOrder.DESC);
-        searchSourceBuilder.sort("informationCode.keyword", SortOrder.DESC);
-        searchSourceBuilder.size(10);
+        // 排序
+        searchSourceBuilder.sort("expireTime", SortOrder.DESC);
+        // 分页查询
         searchSourceBuilder.from(0);
-        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
-        //查询当前机构的
-        boolQueryBuilder.must(QueryBuilders.matchQuery("orgId", 1))
-                //状态都为可用
-                .must(QueryBuilders.matchQuery("state", 1));
+        searchSourceBuilder.size(10);
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery()
+                // 单字段
+                .must(QueryBuilders.matchQuery("source", "微博视频"))
+                // 多字段查询
+                .must(QueryBuilders.multiMatchQuery("机器人", "rawText", "text"));
+        searchSourceBuilder.query(boolQueryBuilder);
         //高亮配置
         HighlightBuilder highlightBuilder = new HighlightBuilder();
-        highlightBuilder.field("activityName")
-                .field("informationName")
-                .field("tableName")
-                .field("informationContent")
+        highlightBuilder.field("source")
                 .requireFieldMatch(false)
-                .preTags("<span style=\"color:yellowgreen\">").postTags("</span>")
-                //下面这两项,如果你要高亮如文字内容等有很多字的字段,必须配置,不然会导致高亮不全,文章内容缺失等
-                .fragmentSize(800000)
-                .numOfFragments(0);
+                .preTags("<span style=\"color:yellow\">")
+                .postTags("</span>")
+                .fragmentSize(800000).numOfFragments(0);
         searchSourceBuilder.highlighter(highlightBuilder);
-        searchSourceBuilder.query(boolQueryBuilder);
+        // 查询请求，可设置多个index
         SearchRequest request = new SearchRequest("weibo_data");
         request.source(searchSourceBuilder);
-        SearchResponse searchResponse = null;
         try {
-            searchResponse = restHighLevelClient.search(request, RequestOptions.DEFAULT);
+            SearchResponse searchResponse = restHighLevelClient.search(request, RequestOptions.DEFAULT);
             SearchHit[] hits = searchResponse.getHits().getHits();
             return ResponseInfo.createSuccess(hits);
         } catch (IOException e) {
